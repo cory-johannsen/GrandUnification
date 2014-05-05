ff44ffc848988f80846828e40b1c41c268957bd1
Cory Johannsen <cory.a.johannsen@gmail.com>
2014-05-04 18:07:35 -0700

    Extended Guice module class to invoke subclass parameter configuration to minimize code copy and paste when create new applications

--- api/src/unification/configuration/GrandUnificationModule.java
+++ api/src/unification/configuration/GrandUnificationModule.java
@@ -8,11 +8,18 @@ package unification.configuration;
 import java.sql.Connection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
+import javax.validation.Validation;
+import javax.validation.ValidatorFactory;
 
+import com.google.inject.Provides;
+import com.google.inject.matcher.Matchers;
+import com.google.inject.persist.PersistFilter;
+import com.google.inject.persist.jpa.JpaPersistModule;
 import org.apache.shiro.guice.web.GuiceShiroFilter;
 import org.codehaus.jackson.jaxrs.JacksonJsonProvider;
 
@@ -48,13 +55,43 @@ import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
  */
 public abstract class GrandUnificationModule extends JerseyServletModule {
 
+
+    // JPA properties
+    public static final String JPA_HIBERNATE_DIALECT = "hibernate.dialect";
+    public static final String JPA_JDBC_DRIVER = "javax.persistence.jdbc.driver";
+    public static final String JPA_JDBC_URL = "javax.persistence.jdbc.url";
+    public static final String JPA_JDBC_USER = "javax.persistence.jdbc.user";
+    public static final String JPA_JDBC_PASSWORD = "javax.persistence.jdbc.password";
+    public static final String JPA_PERSISTENCE_UNIT = "jpa.persistence.unit";
+
     @Override
     protected void configureServlets() {
         super.configureServlets();
         filter("/*").through(GuiceShiroFilter.class);
+        // Load the persistence module properties from the environment
+        Properties jpaProperties = new Properties();
+        String jpaDialect = System.getProperty(JPA_HIBERNATE_DIALECT);
+        String jpaDriver = System.getProperty(JPA_JDBC_DRIVER);
+        String jpaUrl = System.getProperty(JPA_JDBC_URL);
+        String jpaUser = System.getProperty(JPA_JDBC_USER);
+        String jpaPassword = System.getProperty(JPA_JDBC_PASSWORD);
+        String jpaPersistenceUnit = System.getProperty(JPA_PERSISTENCE_UNIT);
+
+        System.out.println("Using hibernate dialect " + jpaDialect);
+        System.out.println("Using JDBC driver " + jpaDriver);
+        System.out.println("Using JDBC URL " + jpaUrl);
+        System.out.println("Using JDBC user " + jpaUser);
+        System.out.println("Using JDBC password " + jpaPassword);
+        System.out.println("Using JPA persistence unit " + jpaPersistenceUnit);
+
+        install(new JpaPersistModule("jpaPersistenceUnit").properties(jpaProperties));
+        filter("/*").through(PersistFilter.class);
         
         // Bind the InitialContext implementation class to the Context interface using singleton semantics
         bind(Context.class).to(InitialContext.class).in(Singleton.class);
+
+        //Bind SLF4J Logging listener
+        bindListener(Matchers.any(), new SLF4JTypeListener());
 
         // Bind an ObjectMapper provider to support seamless Jasper JSON serialization/deserialization
         bind(ObjectMapperProvider.class).asEagerSingleton();
@@ -75,8 +112,19 @@ public abstract class GrandUnificationModule extends JerseyServletModule {
         parameters.put(ResourceConfig.PROPERTY_CONTAINER_RESPONSE_FILTERS,
                 "com.sun.jersey.api.container.filter.LoggingFilter");
 
+        parameters = configureApplicationParameters(parameters);
+
         serve("/*").with(GuiceContainer.class, parameters);
     }
 
-    protected abstract void configureApplicationResources();
+
+
+    @Provides
+    @Singleton
+    javax.validation.Validator provideValidator() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        return factory.getValidator();
+    }
+
+    protected abstract Map<String, String> configureApplicationParameters(Map<String, String> parameters);
 }
