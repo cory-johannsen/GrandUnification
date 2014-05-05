c55ab4b686a8bdaf664a47dfc43a2081697c7c93
Cory Johannsen <cory.johannsen@vendscreen.com>
2014-05-05 07:56:03 -0700

    Added javadoc

--- entity/src/main/java/unification/entity/dao/DataAccessObject.java
+++ entity/src/main/java/unification/entity/dao/DataAccessObject.java
@@ -14,15 +14,53 @@ import java.util.Map;
  */
 public interface DataAccessObject<K,E> {
 
+    /**
+     * Returns the entity with the given ID
+     * @param id
+     * @return
+     * @throws EntityNotFoundException
+     * @throws DaoException
+     */
     public E loadById(K id) throws EntityNotFoundException, DaoException;
 
+    /**
+     * Returns all entities
+     * @return
+     * @throws EntityNotFoundException
+     * @throws DaoException
+     */
     public List<E> loadAll()  throws EntityNotFoundException, DaoException;
 
+    /**
+     * Returns all entities matching the parameters in the given map
+     * @param parameters
+     * @return
+     * @throws EntityNotFoundException
+     * @throws DaoException
+     */
     public List<E> loadByParameters(Map parameters)  throws EntityNotFoundException, DaoException;
 
+    /**
+     * Creates the entity as a persistent object
+     * @param e
+     * @return
+     * @throws DaoException
+     */
     public E create(E e) throws DaoException;
 
+    /**
+     * Stores the given persistent entity
+     * @param e
+     * @return
+     * @throws DaoException
+     */
     public E store(E e) throws DaoException;
 
+    /**
+     * Remote the given persistent entity
+     * @param e
+     * @return
+     * @throws DaoException
+     */
     public E remove(E e) throws DaoException;
 }
